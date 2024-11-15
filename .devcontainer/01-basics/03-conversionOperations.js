8a4c094cbb7723ac7a1ddf4e321d8afe9ea6ccd7
Kanak <kanaksingh2218@gmail.com>
2024-11-15 09:50:30 +0000

    conversion of datatypes

---
+++ .devcontainer/01-basics/03-conversionOperations.js
@@ -0,0 +1,30 @@
+let score = false
+
+console.log( typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log( typeof valueInNumber);
+
+console.log( valueInNumber)
+
+// 33 = 33
+// 33djdh = NaN
+// true = 1 ; false = 0 ;
+// null = 0  ;
+
+let isLoggedIn = "knak"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 = true ; 0= false
+// "" = false 
+// "kanak" = true
+
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log( typeof (stringNumber));
+
