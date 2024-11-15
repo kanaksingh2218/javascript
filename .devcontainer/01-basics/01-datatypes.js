99f1ddeabc27431fd413943e64bebf18e0b80f06
Kanak <kanaksingh2218@gmail.com>
2024-11-15 09:19:54 +0000

    datatypes

---
+++ .devcontainer/01-basics/01-datatypes.js
@@ -0,0 +1,13 @@
+console.log(typeof "kanak");
+
+// number => 2 to power 53
+// BigInt
+// string = ""
+// Boolean = true/false
+// null = Standalone type
+// undefined =
+// Symbol = unique 
+
+
+// objects 
+// null = objects
---
+++ .devcontainer/01-basics/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 2;
+let accountName = "Kanak";
+var accountEmail = "kanaksingh276@gmail.com";
+var accountPassword = "12345";
+var accountCity = "Mumbai"
+
+
+
+// accountId = "Mumbai";
+
+accountName = "vaibhav";
+accountEmail ="kanakvaibhav@gmail.com";
+accountPassword ="jdsjfgdsj";
+accountCity ="gorakhpur"
+let accountState;
+
+
+console.table([accountName, accountEmail, accountPassword, accountCity,  accountState])
