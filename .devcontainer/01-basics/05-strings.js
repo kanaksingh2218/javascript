b65b884d68c3ede73838aaf0a71cc43a2c89c264
Kanak <kanaksingh2218@gmail.com>
2024-11-19 05:39:37 +0000

    strings

---
+++ .devcontainer/01-basics/05-strings.js
@@ -0,0 +1,41 @@
+const name = "kanak"
+const repoCount = 13
+
+// console.log(name +  repoCount  ); Outdated
+
+console.log(`Hello my name is ${name} + my repoCount is ${repoCount}`);
+
+const gameName = new String ('kanak-fc');
+
+// console.log(gameName[0]);
+// console.log(gameName .__proto__);
+// console.log(gameName .length)
+// console.log(gameName .toUpperCase())
+
+// console.log(gameName .charAt(2))
+// console.log(gameName .indexOf('n'))
+
+// const newString = gameName.substring(0, 4) 
+// console.log(newString);
+
+// const anotherString = gameName.slice(-6, 4) 
+// console.log(anotherString);
+
+// const stringOne = "    hitesh         "
+// console.log(stringOne);
+// console.log(stringOne.trim());
+
+const url = "hhtps://kanak.com/kanak20%singh"
+console.log(url.replace('20%' , "-"));
+console.log(url.includes('vaibhav'));
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
