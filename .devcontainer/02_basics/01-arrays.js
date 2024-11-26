4b886f23221faaf1587687af7cfd2e2d5e66bf02
Kanak <kanaksingh2218@gmail.com>
2024-11-26 09:47:35 +0000

    arrays1

---
+++ .devcontainer/02_basics/01-arrays.js
@@ -0,0 +1,37 @@
+const myArr = [ 0, 1, 2, 3, 4, 5]
+const myArr2 = new Array ( 0, 1, 2, 3, 4)
+// console.log(myArr[3]);
+
+// *************Arrays method*********//
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(8)
+// myArr.shift()
+
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice and splice
+
+console.log("A" , myArr);
+
+const myN1 = myArr.slice(1,3)
+console.log(myN1);
+console.log("B" , myArr);
+
+const myN2 = myArr.splice(1,3)
+console.log(myN2);
+console.log("C" , myArr);
+
+
+
+
+
+
+
