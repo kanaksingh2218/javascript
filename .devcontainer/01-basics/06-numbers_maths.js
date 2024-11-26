b390ba49b13ba00511b3ce9cd524949f1d5772c8
Kanak <kanaksingh2218@gmail.com>
2024-11-26 07:29:37 +0000

    numbers and maths

---
+++ .devcontainer/01-basics/06-numbers_maths.js
@@ -0,0 +1,51 @@
+// const score = 400
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(5));
+
+
+const otherNumber = 125.4456
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+//********************************               MATHS           *************************************************** */
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.67));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+
+// console.log(Math.min(3,4,7,8,11,34));
+// console.log(Math.max(3,4,7,8,11,34));
+
+console.log(Math.random());
+console.log(((Math.random()*10) + 1));
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const max= 20
+const min = 10
+
+console.log(Math.floor(Math.random()* (max - min + 1 ) + min) );
+
+
+
+
+
+
+
+
+
+
+
+
+
