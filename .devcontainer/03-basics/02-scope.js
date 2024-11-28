fc22ea0dab68c476064a6e57f912539de11d7a7d
Kanak <kanaksingh2218@gmail.com>
2024-11-28 05:34:34 +0000

    scope

---
+++ .devcontainer/03-basics/02-scope.js
@@ -0,0 +1,53 @@
+// var c = 3000
+
+let a = 300
+
+if (true) {
+    let a= 10
+const b= 20
+// var c= 30
+// console.log("INNER:" , a);
+
+}
+
+// console.log(a);
+
+// console.log(b);
+
+// console.log(c);
+
+function one() {
+     const username = "kanak" 
+    function two() {
+       const website = "chatgpt"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    // two()
+    
+}
+// one()
+
+if (true) {
+    const username = "kanak"
+    if(username=== "kanak"){
+       const website = " whatsapp"
+    //    console.log(username + website);
+
+    }
+    // console.log(website);
+    
+}
+// // console.log(username);
+// console.log(addone(6));
+
+function addone(num1) {
+    return num1 + 1
+}
+// addone(6)
+addtwo(4)
+const addtwo = function (num2) {
+    return num2 + 2
+}
+
